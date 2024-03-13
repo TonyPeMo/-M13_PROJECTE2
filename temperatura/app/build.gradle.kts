77ebdd07b3cc5e215b0aa6ee6cbd2efc4eda024f
Joshua Sedano Rodríguez <114091298+JoshuaSedano@users.noreply.github.com>
2024-03-13 15:40:37 +0100

    estilos y colores

--- temperatura/app/build.gradle.kts
+++ temperatura/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.temperatura"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.temperatura"
         minSdk = 25
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
