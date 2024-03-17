c4293e1de78f953f9025f3c6bbb329dd2d05acc6
soufian123 <proni10102002@gmail.com>
2024-03-17 14:03:15 +0100

    pc casa

--- temperatura/app/build.gradle.kts
+++ temperatura/app/build.gradle.kts
@@ -44,4 +44,12 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+
+    // https://mvnrepository.com/artifact/com.github.skydoves/colorpickerpreference
+    implementation ("com.github.yukuku:ambilwarna:2.0.1")
+
+
+
 }
--- temperatura/app/src/main/java/com/example/temperatura/Configuracion.kt
+++ temperatura/app/src/main/java/com/example/temperatura/Configuracion.kt
@@ -1,11 +1,51 @@
 package com.example.temperatura
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import yuku.ambilwarna.AmbilWarnaDialog
 
 class Configuracion : AppCompatActivity() {
+    private var defaultColor: Int = 0
+    private lateinit var imagenFrio: ImageView
+    private lateinit var imagenOptimo: ImageView
+    private lateinit var imagenCalor: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configuracion)
+
+        // Inicializa tus vistas
+        imagenFrio = findViewById(R.id.lapiz_color_frio)
+        imagenOptimo = findViewById(R.id.lapiz_color_optimo)
+        imagenCalor = findViewById(R.id.lapiz_color_calor)
+
+        // Define un color por defecto
+        defaultColor = ContextCompat.getColor(this, R.color.azul)
+
+        // Establece listeners para las imágenes
+        imagenFrio.setOnClickListener { openColorPicker(imagenFrio) }
+        imagenOptimo.setOnClickListener { openColorPicker(imagenOptimo) }
+        imagenCalor.setOnClickListener { openColorPicker(imagenCalor) }
+    }
+
+    private fun openColorPicker(imagen: ImageView) {
+        val ambilWarnaDialog = AmbilWarnaDialog(
+            this,
+            defaultColor,
+            object : AmbilWarnaDialog.OnAmbilWarnaListener {
+                override fun onCancel(dialog: AmbilWarnaDialog?) {
+                    // No es necesario hacer nada si se cancela
+                }
+
+                override fun onOk(dialog: AmbilWarnaDialog?, color: Int) {
+                    defaultColor = color
+                    // Cambia el color de fondo del óvalo
+                    imagen.background.setColorFilter(defaultColor, PorterDuff.Mode.SRC_ATOP)
+                }
+            })
+        ambilWarnaDialog.show()
     }
 }
