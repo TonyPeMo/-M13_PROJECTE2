8add395cd815198103f0f9b0c3ed23896336a5a0
Soufian123 <proni10102002@gmail.com>
2024-03-21 15:12:15 +0100

    portartil

---
+++ temperatura/app/src/main/java/com/example/temperatura/Graficas.kt
@@ -0,0 +1,11 @@
+package com.example.temperatura
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Graficas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_graficas)
+    }
+}
