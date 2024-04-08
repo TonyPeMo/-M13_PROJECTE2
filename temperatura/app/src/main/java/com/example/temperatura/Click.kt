b92fd4d2b1fe8ea20d1f0effe6950b1e51cf817b
TonyPeMo <tonypm2002@gmail.com>
2024-04-08 16:31:21 +0200

    Creacion de la clase click y correcion de errores

---
+++ temperatura/app/src/main/java/com/example/temperatura/Click.kt
@@ -0,0 +1,43 @@
+package com.example.temperatura
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Click : AppCompatActivity(){
+
+    fun toLogin(view: View) {
+        val intent = Intent(this, Login::class.java).apply{}
+        startActivity(intent);
+    }
+
+    fun toPlanta(view: View) {
+        val intent = Intent(this, PlanoPlanta::class.java).apply{}
+        startActivity(intent);
+    }
+
+    fun toGrafico(view: View) {
+        val intent = Intent(this, Graficas::class.java).apply{}
+        startActivity(intent);
+    }
+
+    fun toConfiguracion(view: View) {
+        val intent = Intent(this, Configuracion::class.java).apply {}
+        startActivity(intent);
+    }
+
+    //fun toNotificacion(view: View) {
+    //    val intent = Intent(this, Notificacion::class.java).apply {}
+    //    startActivity(intent);
+    //}
+
+    fun toInicio(view: View) {
+        val intent = Intent(this, PantallaInicio::class.java).apply {}
+        startActivity(intent);
+    }
+}
