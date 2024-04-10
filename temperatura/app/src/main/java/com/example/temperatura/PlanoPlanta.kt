a769e1cd7c6529308a2cab9f072674952637ac6a
Joshua Sedano Rodríguez <114091298+JoshuaSedano@users.noreply.github.com>
2024-04-10 15:03:30 +0200

    layout planta acabado donde se pintan las aulas en función de la temperatura

--- temperatura/app/src/main/java/com/example/temperatura/PlanoPlanta.kt
+++ temperatura/app/src/main/java/com/example/temperatura/PlanoPlanta.kt
@@ -1,11 +1,39 @@
 package com.example.temperatura
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class PlanoPlanta : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    // HashMap para almacenar las temperaturas por habitación
+    private val tempAulas: HashMap<String, Double> = HashMap()
+
+    override fun onCreate(savedInstanceState: Bundle?) {1
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plano_planta)
+
+        // Agrega las temperaturas para cada aula
+        tempAulas["aula03"] = 23.0
+        tempAulas["aula04"] = 25.0
+        tempAulas["aulaAteca"] = 28.0
+        tempAulas["aula2"] = 18.0
+        tempAulas["aula1"] = 22.0
+
+        // Asigna los colores a las habitaciones basados en la temperatura
+        for ((aula, temperatura) in tempAulas) {
+            val imageViewHabitacion = findViewById<ImageView>(resources.getIdentifier(aula, "id", packageName))
+            imageViewHabitacion.setBackgroundColor(getColorFromTemperature(temperatura))
+        }
+    }
+
+    // Método para determinar el color en función de la temperatura
+    private fun getColorFromTemperature(temperature: Double): Int {
+        return when {
+            temperature < 20 -> Color.parseColor("#751C3AFF") // Azul
+            temperature in 20.0..24.0 -> Color.parseColor("#7500FF00") // Verde
+            else -> Color.parseColor("#75ff0000") // Rojo
+        }
     }
 }
