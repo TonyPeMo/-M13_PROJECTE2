b39d366e83326f8483f10ef579740e152ef2fee5
Soufian123 <proni10102002@gmail.com>
2024-05-15 15:08:23 +0200

    portartil

--- temperatura/app/build.gradle.kts
+++ temperatura/app/build.gradle.kts
@@ -62,7 +62,5 @@ dependencies {
 
     // GRAFICAS
     implementation ("org.quanqi:android-holo-graph:0.1.0")
-    implementation("mysql:mysql-connector-java:8.0.28")
-
 
 }
--- temperatura/app/src/main/java/com/example/temperatura/Login.kt
+++ temperatura/app/src/main/java/com/example/temperatura/Login.kt
@@ -1,6 +1,5 @@
 package com.example.temperatura
 
-import MySQLDatabase
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -28,27 +27,7 @@ class Login : AppCompatActivity() {
 
     fun toInicio(view: View) {
         val intent = Intent(this, PantallaInicio::class.java)
-
-        // Datos de conexión
-        val url = "jdbc:mysql://192.168.19.73:3307/temperatura"
-        val username = "root"
-        val password = "root"
-
-        try {
-            // Crear conexión y operaciones en la base de datos
-            val db = MySQLDatabase(url, username, password)
-            db.connect()
-            db.createTables()
-            db.insertDefaultValues()
-            db.disconnect()
-
-            // Iniciar la siguiente actividad
-            startActivity(intent)
-        } catch (e: SQLException) {
-            // Manejar cualquier error de conexión a la base de datos
-            Log.e("Error de conexión", e.message ?: "Error desconocido al conectar a la base de datos")
-            // Aquí puedes mostrar un mensaje de error al usuario si lo deseas
-        }
+        startActivity(intent)
     }
 
 }
